fe23608838414562ca6dfc82de0b1b0a87dfea8e
Soliha <solyatursuntasheva@gmail.com>
2023-07-25 23:45:13 +0500

    Add: fonts and colors

---
+++ src/styles/colors.js
@@ -0,0 +1,40 @@
+export const primary = {
+  100: "#0177FB",
+  75: "#4199FC",
+  50: "#7FBAFC",
+  25: "#BFDDFE",
+  12: "#E0EEFE",
+}
+
+export const success = {
+  100: "#00D23B",
+  75: "#40DD6C",
+  50: "#7FE89C",
+  25: "#BFF3CE",
+  12: "#F3FCE6",
+}
+
+export const warning = {
+  100: "#FED700",
+  75: "#FEE140",
+  50: "#FEEA7F",
+  25: "#FEF5BF",
+  12: "#FEFAE0",
+}
+
+export const black = {
+  100: "#1C1917",
+  75: "#555351",
+  50: "#8D8C8B",
+  25: "#C6C5C5",
+  12: "#C6C5C5",
+}
+
+export const neo = {
+  100: "#F2F3F7",
+  75: "#F9F9FB"
+}
+
+export const white = {
+  main: "#FFFFFF"
+}
