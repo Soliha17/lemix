3317555c2ebbb71e1ce4254221caf248e6576d2b
Soliha <solyatursuntasheva@gmail.com>
2023-07-25 23:09:19 +0500

    Add: router and folder structure of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import {  Route, Routes } from "react-router-dom";
+import Home from "./ui/pages/Home/Home";
+import NotFound from "./ui/pages/NotFound/NotFound";
+
 function App() {
   return (
-    <div className="container h-screen w-screen bg-red-400 text-center">
-      <h1 className="text-5xl font-bold underline hover:text-blue-600">Hello World!</h1>
-    </div>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
