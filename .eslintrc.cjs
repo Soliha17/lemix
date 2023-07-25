f066b26deea7b8628782dd689ed2612579bc9ad6
Soliha <solyatursuntasheva@gmail.com>
2023-07-25 18:23:10 +0500

    Setup project!

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,11 +10,9 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh','tailwindcss'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    "tailwindcss/classnames-order": "error",
   },
-}
+};
