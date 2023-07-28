da65d225fc94dead37311ad4ef63b1a7d82cf08d
Soliha <solyatursuntasheva@gmail.com>
2023-07-28 10:28:28 +0500

    Refactor: Statistics section to general

--- src/ui/components/Sections/Statistics.jsx
+++ src/ui/components/Sections/Statistics.jsx
@@ -1,24 +1,46 @@
 import { Grid } from '@mui/material';
+
 import StatisticCard from '../StatisticCard/StatisticCard';
 import SectionName from './SectionName';
+
+const statisticDatas = [
+  {
+    id: 1,
+    count: '456 000+',
+    text: 'Foydalanuvchilarimiz qamrovi',
+  },
+  {
+    id: 2,
+    count: '56+',
+    text: 'Hududlar',
+  },
+  {
+    id: 3,
+    count: '556+',
+    text: 'Kompaniyalar',
+  },
+  {
+    id: 4,
+    count: '717+',
+    text: 'Kompaniyalar',
+  },
+];
 
 const Statistics = () => {
   return (
     <div className="mt-16 md:mt-24 lg:mt-20">
       <SectionName name="Statistikamiz" />
       <Grid container rowSpacing={3} mt={4} columnSpacing={{ xs: 2, sm: 3, md: 3 }}>
-        <Grid item xs={12} sm={6} lg={3}>
-          <StatisticCard />
-        </Grid>
-        <Grid item xs={12} sm={6} lg={3}>
-          <StatisticCard />
-        </Grid>
-        <Grid item xs={12} sm={6} lg={3}>
-          <StatisticCard />
-        </Grid>
-        <Grid item xs={12} sm={6} lg={3}>
-          <StatisticCard />
-        </Grid>
+        {statisticDatas.map((statistic) => (
+          <Grid item key={statistic.id} xs={12} sm={6} lg={3}>
+            <StatisticCard
+              count={statistic.count}
+              text={statistic.text}
+              textColor="text-primary-100"
+              bgColor="bg-primary-12"
+            />
+          </Grid>
+        ))}
       </Grid>
     </div>
   );
--- src/ui/components/StatisticCard/StatisticCard.jsx
+++ src/ui/components/StatisticCard/StatisticCard.jsx
@@ -1,10 +1,25 @@
-const StatisticCard = () => {
+import PropTypes from 'prop-types';
+
+const StatisticCard = ({ count, text, textColor, bgColor }) => {
   return (
-    <div className="flex flex-col gap-8 rounded-2xl bg-primary-12 p-5">
-      <h2 className="text-3xl font-medium text-primary-100 lg:text-5xl">456 000+</h2>
-      <p className="p1 text-black-100">Foydalanuvchilarimiz qamrovi</p>
+    <div
+      className={`${bgColor} flex flex-col gap-8 rounded-2xl bg-no-repeat p-5`}
+      style={{
+        backgroundImage: 'url("src/assets/images/line__statistic-card.svg")',
+        backgroundPosition: 'top right',
+      }}
+    >
+      <h2 className={`text-3xl font-medium ${textColor} lg:text-5xl`}>{count}</h2>
+      <p className="p1 text-black-100">{text}</p>
     </div>
   );
 };
 
 export default StatisticCard;
+
+StatisticCard.propTypes = {
+  count: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  textColor: PropTypes.string.isRequired,
+  bgColor: PropTypes.string.isRequired,
+};
