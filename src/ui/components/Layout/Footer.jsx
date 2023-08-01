c008cc75a1eac612e4e5d63090716aae3c23ec67
Soliha <solyatursuntasheva@gmail.com>
2023-08-01 20:35:31 +0500

    Make: footer

--- src/ui/components/Layout/Footer.jsx
+++ src/ui/components/Layout/Footer.jsx
@@ -1,8 +1,58 @@
+import youTube from 'src/assets/images/you-tube.svg';
+import telegram from 'src/assets/images/telegram.svg';
+import instagram from 'src/assets/images/instagram.svg';
+import facebook from 'src/assets/images/facebook.svg';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div
+      className="bg-black-100 bg-no-repeat pb-6 pt-8 md:pb-8 md:pt-14"
+      style={{
+        backgroundImage: 'url("src/assets/images/line__footer.png")',
+        backgroundPosition: 'top right',
+      }}
+    >
+      <div className="mx-auto max-w-7xl px-5 text-neo-75 md:px-10">
+        <div className="mb-10 flex flex-wrap justify-between gap-11 sm:justify-start md:mb-14 md:gap-36">
+          <div>
+            <h6 className="mb-3 text-base font-bold text-white-main">Lemix</h6>
+            <ul className="flex cursor-pointer flex-col gap-3 text-sm text-neo-100">
+              <li>Mahsulotlar</li>
+              <li>Yechimlar</li>
+              <li>Resurslar</li>
+            </ul>
+          </div>
+          <div>
+            <h6 className="mb-3 text-base font-bold text-white-main">A&apos;loqa</h6>
+            <ul className="flex cursor-pointer flex-col gap-3 text-sm text-neo-100">
+              <li>+998-(00)-000-00-00</li>
+              <li>Lemix@gmail.com</li>
+            </ul>
+          </div>
+          <div>
+            <h6 className="mb-3 text-base font-bold text-white-main">Ijtimoiy Tarmoqlar</h6>
+            <ul className="flex cursor-pointer gap-3">
+              <li>
+                <img src={youTube} alt="youTube" />
+              </li>
+              <li>
+                <img src={telegram} alt="telegram" />
+              </li>
+              <li>
+                <img src={instagram} alt="instagram" />
+              </li>
+              <li>
+                <img src={facebook} alt="facebook" />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <p className="border-t border-neo-75 pt-6 text-base font-medium md:pt-8">
+          &copy; 2023 Lemix
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/ui/pages/Home/Home.jsx
+++ src/ui/pages/Home/Home.jsx
@@ -2,6 +2,7 @@
 
 import AboutCard from 'src/ui/components/Cards/AboutCard';
 import HeroHome from 'src/ui/components/Heros/HeroHome/HeroHome';
+import Footer from 'src/ui/components/Layout/Footer';
 import Header from 'src/ui/components/Layout/Header/Header';
 import FeedBacks from 'src/ui/components/Sections/FeedBacks';
 import OpportunityHome from 'src/ui/components/Sections/OpportunityHome';
@@ -11,15 +12,18 @@ import Statistics from 'src/ui/components/Sections/Statistics';
 const Home = () => {
   // const {t} = useTranslation()
   return (
-    <div className="mx-auto max-w-7xl px-5 md:px-10">
-      <Header />
-      <HeroHome />
-      <ProductHome />
-      <Statistics />
-      <FeedBacks />
-      <OpportunityHome />
-      <AboutCard />
-    </div>
+    <>
+      <div className="mx-auto max-w-7xl px-5 md:px-10">
+        <Header />
+        <HeroHome />
+        <ProductHome />
+        <Statistics />
+        <FeedBacks />
+        <OpportunityHome />
+        <AboutCard />
+      </div>
+      <Footer />
+    </>
   );
 };
 
