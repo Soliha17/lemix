f4ae2f8bfb5e9e1435e0a0e4cfc2cde2d814f2f6
Soliha <solyatursuntasheva@gmail.com>
2023-08-10 16:04:08 +0500

    change back structure of vite

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-import path from 'path'; // Import the 'path' module
-
-// Calculate the directory name using import.meta.url
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      src: path.resolve(__dirname, 'src'),
+      src: '/src',
     },
   },
 });
