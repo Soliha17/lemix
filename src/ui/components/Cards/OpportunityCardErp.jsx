d42688fbf308eaed510189cb12ce1cfa3d4bbc30
Soliha <solyatursuntasheva@gmail.com>
2023-08-12 11:43:42 +0500

    Make: opportunity shadows of erp page

--- src/ui/components/Cards/OpportunityCardErp.jsx
+++ src/ui/components/Cards/OpportunityCardErp.jsx
@@ -7,7 +7,7 @@ const OpportunityCardErp = () => {
   return (
     <div className="w-full rounded-3xl bg-primary-12 p-6 sm:p-9 xl:p-20">
       <SectionName name="Imkoniyatlar" />
-      <div className="mt-10 flex flex-col-reverse items-center gap-10 rounded-3xl bg-white-main p-6 sm:p-9 md:flex-row xl:p-16">
+      <div className="opp-card__erp mt-20 flex flex-col-reverse items-center gap-10 rounded-3xl bg-white-main p-6 sm:p-9 md:flex-row xl:p-16">
         <div className="w-full md:w-1/2">
           <h3 className="mb-4 text-xl font-semibold text-black-100 sm:text-4xl md:mb-6">Sotuv</h3>
           <p className="text-base leading-6 text-black-75">
@@ -17,7 +17,7 @@ const OpportunityCardErp = () => {
           </p>
         </div>
         <div className="relative">
-          <p className="absolute -right-8 -top-12 rotate-12 rounded-2xl bg-primary-25 px-7 py-2 text-white-main md:-right-5 md:-top-3">
+          <p className="absolute -right-8 -top-12 z-10 rotate-12 rounded-2xl bg-primary-25 px-7 py-2 text-white-main md:-right-5 md:-top-3">
             Sotuv bo&apos;limi <br /> nazorati
           </p>
           <img src={tableImg} alt="tableImg" />
