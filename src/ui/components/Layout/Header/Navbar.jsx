8e8fdeafec78b0c6c3fe91cbfa80fb0cc71294d6
Soliha <solyatursuntasheva@gmail.com>
2023-08-14 11:23:47 +0500

    Add: scroll active to navbar

--- src/ui/components/Layout/Header/Navbar.jsx
+++ src/ui/components/Layout/Header/Navbar.jsx
@@ -1,17 +1,14 @@
-import React, { useEffect } from 'react';
+import { useState, useEffect, useCallback, useMemo } from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 
 const Navbar = () => {
-  const [value, setValue] = React.useState(0);
-  const [isSticky, setIsSticky] = React.useState(false);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  const [value, setValue] = useState(0);
+  const [isSticky, setIsSticky] = useState(false);
 
   const handleTabClick = (sectionId) => {
     const section = document.getElementById(sectionId);
+    console.log(sectionId);
     if (section) {
       section.scrollIntoView({
         behavior: 'smooth',
@@ -19,20 +16,41 @@ const Navbar = () => {
     }
   };
 
-  const handleScroll = () => {
+  const sections = useMemo(
+    () => ['general', 'opportunities', 'advantages', 'feedbacks', 'prices', 'contact'],
+    [],
+  );
+
+  const handleScroll = useCallback(() => {
+    for (let i = 0; i < sections.length; i++) {
+      const section = document.getElementById(sections[i]);
+      if (section) {
+        const rect = section.getBoundingClientRect();
+        if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+          setValue(i);
+        }
+      }
+    }
+
     if (window.scrollY > 0) {
       setIsSticky(true);
     } else {
       setIsSticky(false);
     }
-  };
+  }, [sections]);
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+  }, [handleScroll]);
+
+  const handleChange = (event, newValue) => {
+    console.log(newValue);
+    setValue(newValue);
+    handleTabClick(sections[newValue]);
+  };
 
   return (
     <div
@@ -41,18 +59,19 @@ const Navbar = () => {
       } z-40 flex w-full justify-center border-b border-t border-neo-100 bg-white-main`}
     >
       <Tabs
+        key={value}
         value={value}
         onChange={handleChange}
         variant="scrollable"
         scrollButtons={false}
         aria-label="scrollable prevent tabs example"
       >
-        <Tab label="Umumiy" onClick={() => handleTabClick('general')} />
-        <Tab label="Imkoniyatlar" onClick={() => handleTabClick('opportunities')} />
-        <Tab label="Fikrlar" onClick={() => handleTabClick('feedbacks')} />
-        <Tab label="Afzalliklar" onClick={() => handleTabClick('advantages')} />
-        <Tab label="Narxlar" onClick={() => handleTabClick('prices')} />
-        <Tab label="Bog'lanish" onClick={() => handleTabClick('contact')} />
+        <Tab label="Umumiy" />
+        <Tab label="Imkoniyatlar" />
+        <Tab label="Afzalliklar" />
+        <Tab label="Fikrlar" />
+        <Tab label="Narxlar" />
+        <Tab label="Bog'lanish" />
       </Tabs>
     </div>
   );
