6303ca6725e67dd97a2bdd91a461254c76df04db
Soliha <solyatursuntasheva@gmail.com>
2023-08-14 22:53:35 +0500

    Refactor: some styles

--- src/ui/components/Layout/Header/Navbar.jsx
+++ src/ui/components/Layout/Header/Navbar.jsx
@@ -24,11 +24,9 @@ const Navbar = () => {
   const handleScroll = useCallback(() => {
     for (let i = 0; i < sections.length; i++) {
       const section = document.getElementById(sections[i]);
-      // console.log(section);
       if (section) {
         const rect = section.getBoundingClientRect();
         if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-          console.log(section);
           setValue(i);
         }
       }
