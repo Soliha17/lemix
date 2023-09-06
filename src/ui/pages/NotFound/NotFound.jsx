5d8bd4c8cf5a7e68d4b17591a399b35b172eb19c
Soliha <solyatursuntasheva@gmail.com>
2023-09-06 12:18:42 +0500

    Make: not found page

--- src/ui/pages/NotFound/NotFound.jsx
+++ src/ui/pages/NotFound/NotFound.jsx
@@ -1,8 +1,208 @@
+import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default NotFound
+  function goBack() {
+    navigate(-1);
+  }
+
+  function goToHome() {
+    navigate('/');
+  }
+
+  return (
+    <section className="bg-white dark:bg-gray-900 ">
+      <div className="container mx-auto flex min-h-screen items-center justify-center px-6 py-12">
+        <div className="w-full">
+          <div className="mx-auto flex max-w-lg flex-col items-center text-center">
+            <p className="text-sm font-medium text-blue-500 dark:text-blue-400">404 error</p>
+            <h1 className="dark:text-white mt-3 text-2xl font-semibold text-gray-800 md:text-3xl">
+              We lost this page
+            </h1>
+            <p className="mt-4 text-gray-500 dark:text-gray-400">
+              We searched high and low, but couldn&apos;t find what you&apos;re looking for.
+              Let&apos;s find a better place for you to go.
+            </p>
+
+            <div className="mt-6 flex w-full shrink-0 items-center gap-x-3 sm:w-auto">
+              <button
+                className="bg-white flex w-1/2 items-center justify-center gap-x-2 rounded-lg border px-5 py-2 text-sm text-gray-700 transition-colors duration-200 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-200 dark:hover:bg-gray-800 sm:w-auto"
+                onClick={goBack}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-5 w-5 rtl:rotate-180"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+                  />
+                </svg>
+
+                <span>Go back</span>
+              </button>
+
+              <button
+                className="text-white w-1/2 shrink-0 rounded-lg bg-primary-100 px-5 py-2 text-sm tracking-wide text-white-main transition-colors duration-200 hover:bg-primary-75 dark:bg-blue-600 dark:hover:bg-blue-500 sm:w-auto"
+                onClick={goToHome}
+              >
+                Take me home
+              </button>
+            </div>
+          </div>
+
+          <div className="mx-auto mt-8 grid w-full max-w-6xl grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            <div className="rounded-lg bg-blue-50 p-6 dark:bg-gray-800">
+              <span className="text-gray-500 dark:text-gray-400">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+                  />
+                </svg>
+              </span>
+
+              <h3 className="mt-6 font-medium text-gray-700 dark:text-gray-200 ">Documentation</h3>
+
+              <p className="mt-2 text-gray-500 dark:text-gray-400 ">
+                Dive in to learn all about our product.
+              </p>
+
+              <a
+                href="#"
+                className="mt-4 inline-flex items-center gap-x-2 text-sm text-blue-500 hover:underline dark:text-blue-400"
+              >
+                <span>Start learning</span>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-5 w-5 rtl:rotate-180"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                  />
+                </svg>
+              </a>
+            </div>
+
+            <div className="rounded-lg bg-blue-50 p-6 dark:bg-gray-800">
+              <span className="text-gray-500 dark:text-gray-400">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+                  />
+                </svg>
+              </span>
+
+              <h3 className="mt-6 font-medium text-gray-700 dark:text-gray-200 ">Our blog</h3>
+
+              <p className="mt-2 text-gray-500 dark:text-gray-400 ">
+                Read the latest posts on our blog.
+              </p>
+
+              <a
+                href="#"
+                className="mt-4 inline-flex items-center gap-x-2 text-sm text-blue-500 hover:underline dark:text-blue-400"
+              >
+                <span>View lastest posts</span>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-5 w-5 rtl:rotate-180"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                  />
+                </svg>
+              </a>
+            </div>
+
+            <div className="rounded-lg bg-blue-50 p-6 dark:bg-gray-800">
+              <span className="text-gray-500 dark:text-gray-400">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
+                  />
+                </svg>
+              </span>
+
+              <h3 className="mt-6 font-medium text-gray-700 dark:text-gray-200 ">Chat to us</h3>
+
+              <p className="mt-2 text-gray-500 dark:text-gray-400 ">
+                Can’t find what you’re looking for?
+              </p>
+
+              <a
+                href="#"
+                className="mt-4 inline-flex items-center gap-x-2 text-sm text-blue-500 hover:underline dark:text-blue-400"
+              >
+                <span>Chat to our team</span>
+
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-5 w-5 rtl:rotate-180"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default NotFound;
