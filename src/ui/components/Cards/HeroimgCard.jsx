ac1c16fedb4c131915262c16b09cde85c241c70a
Soliha <solyatursuntasheva@gmail.com>
2023-09-22 17:23:45 +0500

    Reoptimize: add width and height to images of hero-home

--- src/ui/components/Cards/HeroimgCard.jsx
+++ src/ui/components/Cards/HeroimgCard.jsx
@@ -9,7 +9,7 @@ const HeroimgCard = ({ classname, title, img }) => {
       className={`${classname} flex w-64 justify-center gap-4 rounded-2xl bg-white-main bg-opacity-30 p-4 backdrop-blur`}
     >
       <h6 className="font-medium text-white-main">{t(title)}</h6>
-      <img src={img} className="img__hero-img-card" alt={img} />
+      <img src={img} className="img__hero-img-card" alt={img} width={224} height={150} />
     </div>
   );
 };
