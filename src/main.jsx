fd58deb963a9944a38e3cb75b74a671a43c9edfa
Soliha <solyatursuntasheva@gmail.com>
2023-09-22 18:31:04 +0500

    Add: sentry

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,24 @@ import './index.css';
 import App from './App.jsx';
 import i18n from './i18n';
 
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+  dsn: 'https://8e61a070e18269ac12d4fc0a8b32d997@o4505924207640576.ingest.sentry.io/4505924322918400',
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <I18nextProvider i18n={i18n}>
